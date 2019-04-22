1fe127340fcd043f05b0c3e110a95dd6065ae7c5
ihurley19 <ihurley19@kentdenver.org>
2019-04-22 15:01:36 -0600

    IC-2017.2.2 <ihurley19@ihurley19.local Create baseRefactoring.xml, project.default.xml, git.xml, other.xml, Class.java, jdk.table.xml, debugger.renderers.xml, Default.xml, find.xml, ui.lnf.xml, laf.xml, filetypes.xml, colors.scheme.xml, notifications.xml, keymap.xml, path.macros.xml, editor.xml, debugger.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,13 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+* Name: Izzy Hurley
+* Lab Name: ${NAME}
+* Lab Purpose: The goal of ${NAME} is to create a program 
+* Date: ${DATE}
+* Collaborators: None
+* ON MY HONOR: IH
+*/
+
+public class ${NAME} {
+}
